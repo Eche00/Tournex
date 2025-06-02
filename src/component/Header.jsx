60f6016f33569b28115c7c7a4c0d910a4775cb0b
Eche00 <echeeze956@gmail.com>
2025-06-02 16:17:36 +0100

    Header and sidebar

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="shadow-b-black shadow-2xl  fixed top-0 left-0 w-full bg-white z-50">
+      <div className=" flex items-center justify-between py-[10px] sm:px-[50px] px-[10px] ">
+        {/* logo */}
+        <section>
+          <img
+            src=""
+            alt="//"
+            className="w-[50px] h-[50px] object-cover bg-gray-300 rounded-full"
+          />
+        </section>
+
+        {/* auth  */}
+        <section className="sm:flex items-center gap-[16px] hidden">
+          <Link className=" flex items-center justify-center w-[100px] border-[0.8px] border-gray-500 text-gray-500 hover:bg-blue-600 hover:border-none hover:text-white  text-[14px] font-bold py-[8px] rounded-[10px] cursor-pointer hover:scale-105 transition-all duration-300">
+            Login
+          </Link>
+          <Link className=" flex items-center justify-center w-[100px] bg-blue-600 py-[8px] text-white text-[14px] font-bold  rounded-[10px] cursor-pointer hover:scale-105 transition-all duration-300">
+            SignUp
+          </Link>
+        </section>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,7 +1,80 @@
 import React from "react";
+import { NavLink, useLocation } from "react-router";
+import HouseIcon from "@mui/icons-material/House";
+import NewspaperIcon from "@mui/icons-material/Newspaper";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import FindReplaceIcon from "@mui/icons-material/FindReplace";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <div className=" w-[300px] h-[100vh] bg-blue-600">
+      <nav className=" flex flex-col   text-[14px] px-[20px] py-[10px] gap-[5px]">
+        {/* logo */}
+        <section className=" flex items-center gap-[4px]  py-[26px]">
+          <img
+            src=""
+            alt="//"
+            className="w-[48px] h-[48px] object-cover bg-white rounded-full"
+          />
+          <h1 className=" text-[24px] font-[600] text-white">UNN Sports Hub</h1>
+        </section>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "flex items-center  gap-[5px] bg-[#1e222b]  font-extrabold py-[10px] px-[12px] rounded-[10px] text-white transition-all duration-300 "
+              : "flex items-center  text-gray-300 font-bold py-[10px] px-[12px] rounded-[10px] transition-all duration-300 hover:bg-[#1e222b38] scale-95 "
+          }>
+          {" "}
+          <HouseIcon /> <span>Home</span>
+        </NavLink>
+        <NavLink
+          to="/news"
+          className={({ isActive }) =>
+            isActive
+              ? "flex items-center  gap-[5px] bg-[#1e222b]  font-extrabold py-[10px] px-[12px] rounded-[10px] text-white transition-all duration-300 "
+              : "flex items-center  text-gray-300 font-bold py-[10px] px-[12px] rounded-[10px] transition-all duration-300 hover:bg-[#1e222b38] scale-95"
+          }>
+          {" "}
+          <NewspaperIcon /> <span>News</span>
+        </NavLink>
+
+        <NavLink
+          to="/matches"
+          className={({ isActive }) =>
+            isActive
+              ? "flex items-center  gap-[5px] bg-[#1e222b]  font-extrabold py-[10px] px-[12px] rounded-[10px] text-white transition-all duration-300 "
+              : "flex items-center  text-gray-300 font-bold py-[10px] px-[12px] rounded-[10px] transition-all duration-300 hover:bg-[#1e222b38] scale-95"
+          }>
+          {" "}
+          <EmojiEventsIcon /> <span>Matches</span>
+        </NavLink>
+
+        <NavLink
+          to="/search"
+          className={({ isActive }) =>
+            isActive
+              ? "flex items-center  gap-[5px] bg-[#1e222b]  font-extrabold py-[10px] px-[12px] rounded-[10px] text-white transition-all duration-300 "
+              : "flex items-center  text-gray-300 font-bold py-[10px] px-[12px] rounded-[10px] transition-all duration-300 hover:bg-[#1e222b38] scale-95"
+          }>
+          {" "}
+          <FindReplaceIcon /> <span>Search</span>
+        </NavLink>
+
+        <NavLink
+          to="/profile"
+          className={({ isActive }) =>
+            isActive
+              ? "flex items-center  gap-[5px] bg-[#1e222b]  font-extrabold py-[10px] px-[12px] rounded-[10px] text-white transition-all duration-300 "
+              : "flex items-center  text-gray-300 font-bold py-[10px] px-[12px] rounded-[10px] transition-all duration-300 hover:bg-[#1e222b38] scale-95"
+          }>
+          {" "}
+          <ManageAccountsIcon /> <span>Profile</span>
+        </NavLink>
+      </nav>
+    </div>
+  );
 }
 
 export default Sidebar;
