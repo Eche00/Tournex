7820243e8b568ed273d4ee6e71c8d8af46a15960
Eche00 <echeeze956@gmail.com>
2025-06-02 01:33:22 +0100

    tailwind configs

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Route, Routes } from "react-router";
+
+function App() {
+  return (
+    <div className=" text-black ">
+      <Routes>
+        <Route index></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
